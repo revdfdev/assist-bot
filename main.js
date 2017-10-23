f0a847e5d50927ade05660c9de44480416b1bd3c
thepunkprogrammer <mkodekar1990@gmail.com>
2017-10-23 17:32:18 +0530

    Change error text

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ rest.use(parser.urlencoded({
 rest.use(parser.json());
 
 rest.post('/google_now_bot', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.message ? req.body.result.parameters.message : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.message ? req.body.result.parameters.message : "I cannot get you, please speak again."
         res.json({
             message: speech,
             displayText: speech,
